fb132fa537b83a416bd5433421824d71a1fbf8ed
McLDY <mcldy00@oitlook.com>
2025-05-09 13:55:42 +0800

    commit

--- app.js
+++ app.js
@@ -7,23 +7,10 @@ app.get('/', function (req, res) {
    res.sendFile(`${__dirname}/index.html`)
 })
  
- 
 //  POST 请求
-app.post('login', function (req, res) {
+app.post('/login', function (req, res) {
    console.log("主页 POST 请求");
    res.send('Hello POST');
-})
- 
-//  /del_user 页面响应
-app.get('/del_user', function (req, res) {
-   console.log("/del_user 响应 DELETE 请求");
-   res.send('删除页面');
-})
- 
-//  /list_user 页面 GET 请求
-app.get('/list_user', function (req, res) {
-   console.log("/list_user GET 请求");
-   res.send('用户列表页面');
 })
  
 // 对页面 abcd, abxcd, ab123cd, 等响应 GET 请求
